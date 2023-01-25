41dd7874d344cb5a3da9efd33d311d3d79995b15
Sugiarto <emailgiar@gmail.com>
2023-01-25 13:18:46 +0700

    perubahan title

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Aplikasi Sistem Informasi Warga</title>
+	<title>Aplikasi Sistem Informasi Warga Gondrong Kenanga</title>
 	<link rel="icon" href="dist/img/kenanga.png">
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
